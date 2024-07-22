ad6d900fe0e34b2bb10c544742e2464d31da78bb
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2024-07-22 15:55:49 +0200

    admin: new ui for new models

--- src/carpeCalendar/admin.py
+++ src/carpeCalendar/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import Event
+from .models import Category
+from .models import Place
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['title', 'location', 'category', 'validated']
+    list_display = ['title', 'location', 'category', 'validated', 'get_dates']
     list_editable = ["validated"]
     list_filter = ["validated", "category"]
     search_fields = ["title", "location"]
@@ -17,4 +19,25 @@ class EventAdmin(admin.ModelAdmin):
     def invalidate_selected_events(self, request, queryset):
         queryset.update(validated=False)
 
+    def get_dates(self, obj):
+        return "|\n".join([str(date) for date in obj.eventdates_set.all()])
+
+class EventDatesAdmin(admin.ModelAdmin):
+    list_display = ['event', 'start', 'end']
+    list_filter = ['event']
+    search_fields = ['event']
+    list_editable = ['start', 'end']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent']
+    list_filter = ['parent']
+    list_editable = ['parent']
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'latitude', 'longitude']
+    list_editable = ['address', 'latitude', 'longitude']
+
 admin.site.register(Event, EventAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
