a4a5052ef3ea9584b3d9bc56960b8912d1164cd3
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2024-07-22 15:57:56 +0200

    populate: adapt to new models

--- src/populate.py
+++ src/populate.py
@@ -1,15 +1,39 @@
 import json
 
-from carpeCalendar.models import Event
+from carpeCalendar.models import Event, EventDates, Category, Place
 
 content = json.load(open("../results.json"))
 
+other_location = Place(name="Autre")
+other_location.save()
+
+jean_vilar = Place(name="Théâtre Jean Vilar")
+jean_vilar.save()
+
+theatre_blocry = Place(name="Théâtre Blocry")
+theatre_blocry.save()
+
+placet = Place(name="Salle Placet")
+placet.save()
+
 for event in content:
+    if Category.objects.filter(name=event["infos"]["category"]).exists():
+        category = Category.objects.get(name=event["infos"]["category"])
+    else:
+        category = Category(name=event["infos"]["category"])
+        category.save()
+
+    if Place.objects.filter(name=event["infos"]["place"]).exists():
+        location = Place.objects.get(name=event["infos"]["place"])
+    else:
+        location = other_location
+
     event_obj = Event(
         title=event["name"],
         description=event["description"],
         location=event["infos"]["place"],
-        category=event["infos"]["category"],
+        saved_location=location,
+        category=category,
     )
 
     event_obj.save()
