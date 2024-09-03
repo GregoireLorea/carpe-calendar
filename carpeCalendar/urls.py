a8e1bb8b782974260a3644170515b8923de544ed
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2024-09-03 14:22:34 +0200

    urls: redirect homepage

--- carpeCalendar/urls.py
+++ carpeCalendar/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('calendar/', views.calendar, name='calendar'),
     path('event/<int:event_id>/', views.event, name='event'),
     path('events/', views.events, name='events'),
