89930f1fa9cb7fa78b53e34af83b5ada87d6817c
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2024-10-07 14:35:35 +0200

    create_qr_code: create form and qr code gen

---
+++ carpeCalendar/static/js/create_qr_code.js
@@ -0,0 +1,79 @@
+const textAreaData = document.getElementById('qr-code-text');
+const qrCodeImage = document.getElementById('qr-code-image');
+const qrCodeColor = document.getElementById('qr-code-color');
+const qrCodeWidth = document.getElementById('qr-code-width');
+const qrCodeHeight = document.getElementById('qr-code-height');
+const qrCodeBackgroundColor = document.getElementById('qr-code-background-color');
+const qrCodeColorCornerSquare = document.getElementById('qr-code-color-corner-square');
+
+const downloadSVG = document.getElementById('generate-qr-code-svg');
+const downloadPNG = document.getElementById('generate-qr-code-png');
+
+const data = textAreaData.value;
+const color = qrCodeColor.value;
+const width = qrCodeWidth.value;
+const height = qrCodeHeight.value;
+const cornerSquareColor = qrCodeColorCornerSquare.value;
+const backgroundColor = qrCodeBackgroundColor.value;
+
+const qrCode = new QRCodeStyling({
+    width: height,
+    height: width,
+    type: "svg",
+    data: data,
+    dotsOptions: {
+        color: color,
+        type: "rounded"
+    },
+    backgroundOptions: {
+        color: backgroundColor,
+    },
+    imageOptions: {
+        margin: 20,
+    }
+});
+
+qrCode.append(document.getElementById("qr-display"));
+
+const events = [
+    { element: textAreaData, event: 'input', property: 'data' },
+    { element: qrCodeWidth, event: 'input', property: 'width' },
+    { element: qrCodeHeight, event: 'input', property: 'height' },
+    { element: qrCodeColor, event: 'input', property: 'dotsOptions.color' },
+    { element: qrCodeImage, event: 'input', property: 'image', isFile: true },
+    { element: qrCodeBackgroundColor, event: 'input', property: 'backgroundOptions.color' },
+    { element: qrCodeColorCornerSquare, event: 'input', property: 'cornersSquareOptions.color' },
+];
+
+function updateQRCode(property, value) {
+    const updateData = {};
+    const keys = property.split('.');
+    if (keys.length > 1) {
+        updateData[keys[0]] = { [keys[1]]: value };
+    } else {
+        updateData[property] = value;
+    }
+    qrCode.update(updateData);
+}
+
+events.forEach(({ element, event, property, isFile }) => {
+    element.addEventListener(event, () => {
+        if (isFile) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                updateQRCode(property, e.target.result);
+            }
+            reader.readAsDataURL(element.files[0]);
+        } else {
+            updateQRCode(property, element.value);
+        }
+    });
+});
+
+downloadSVG.addEventListener('click', () => {
+    qrCode.download({ extension: "svg" });
+});
+
+downloadPNG.addEventListener('click', () => {
+    qrCode.download({ extension: "png" });
+});
