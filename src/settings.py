05afac72773b997f8d158f7627342212a9d1dd37
Grégoire <gregoire.lorea@gmail.com>
2025-09-02 23:56:41 +0200

    Configuration PostgreSQL en place

--- src/settings.py
+++ src/settings.py
@@ -100,12 +100,23 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
+# Configuration par défaut avec SQLite pour le développement
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Utiliser PostgreSQL en production si DATABASE_URL est définie
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 
 
 # Password validation
